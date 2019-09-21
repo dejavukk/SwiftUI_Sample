a1c701eaa173703b48272c404c2aa9856d567088
junhyuk <dejavukk1@gmail.com>
2019-09-21 10:44:11 +0900

    Initial Commit

---
+++ SwiftUI_Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Sample
+//
+//  Created by JunHyuk on 21/09/2019.
+//  Copyright © 2019 junhyuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
